e9d7b195d28f97dce9d493d6a10b632a24cb3f08
Siyabonga-Zindela <siyabongazindela@outlook.com>
2026-06-23 23:34:48 +0200

    Added send email functionality

--- js/main.js
+++ js/main.js
@@ -48,3 +48,30 @@ items.forEach((el, index) => {
   observer.observe(el);
 });
 
+
+function sendMail(){
+  let emailInputs ={
+    name: document.getElementById("user_name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value
+  }
+
+  const serviceId = "service_mh3xsnz";
+const templateId = "template_m3xxftl";
+
+emailjs.send(serviceId,templateId,emailInputs)
+.then(
+  response =>{
+    document.getElementById("user_name").value ="";
+    document.getElementById("email").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+    console.log(response);
+    alert("Email sent sucessfully!");
+  }
+).catch(err => console.log(err))
+;
+}
+
+
